8ee9d722a7ed6b91170a4b99c1c9290d5134e393
Carmelleonie <sagboleonie7@gmail.com>
2022-12-09 00:59:45 +0100

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+	for idx in range(len(my_list)):
+		if my_list == []:
+			return None
+		else:
+			if my_list[idx] < my_list[idx + 1]:
+				return my_list[idx + 1]
+			else:
+				return my_list[idx]
--- 0x03-python-data_structures/README.md
+++ 0x03-python-data_structures/README.md
@@ -1,1 +1,1 @@
-Write a function that removes all characters c and C from a string.
+Write a function that finds the biggest integer of a list. 
