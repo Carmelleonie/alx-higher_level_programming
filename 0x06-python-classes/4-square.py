f43079d2be91f99f7e41680d9b3adf39dfdcd4e0
Carmelleonie <sagboleonie7@gmail.com>
2023-02-21 23:26:16 +0100

    4-square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""An empty class in Square"""
+
+
+class Square():
+    """Square, an empty class"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter function"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ that returns the current square area"""
+        return self.__size ** 2
