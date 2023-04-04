b2d2e8de9a57c3b1946fdc85ae1e94266d8bea8d
Carmelleonie <sagboleonie7@gmail.com>
2023-04-04 22:08:28 +0100

    0x11-python-network_1

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Get Github User Id based on username and password.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+
+    results = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
+    if results.get('id') is None:
+        print("None")
+    else:
+        print("{}".format(results.get('id')))
