cf815606b52f64f218522a33aba9ebe5ea85014e
Carmelleonie <sagboleonie7@gmail.com>
2023-06-17 18:35:32 +0100

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,8 @@
 """
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
 
 if __name__ == "__main__":
     main()
+
