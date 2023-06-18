738f9599afe6fa7525ccf29c837b388a7dd0b810
Carmelleonie <sagboleonie7@gmail.com>
2023-06-18 17:54:39 +0100

    0-read_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,5 @@ and returns the number of characters written:
 """
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="utf-8") as f:
-         return f.write(text)
+        return f.write(text)
 
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,3 +7,4 @@ and returns the number of characters added
 def append_write(filename="", text=""):
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
+
