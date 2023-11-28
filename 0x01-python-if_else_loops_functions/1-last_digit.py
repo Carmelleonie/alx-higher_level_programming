b4edebf2383a2b51fc81eeccac5b9f5cb096cb58
Carmelleonie <sagboleonie7@gmail.com>
2023-11-29 00:25:56 +0100

    if-elif-else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	last_di = number % -10
+    last_di = number % -10
 else:
-	last_di = number % 10
+    last_di = number % 10
 if last_di > 5:
-	print("Last digit of {} is {} and is greater than 5".format(number, last_di))
+    print("Last digit of {} is {} and is greater than 5".format(number, last_di))
 elif last_di == 0:
-	print("Last digit of {} is {}".format(number, last_di))
+    print("Last digit of {} is {}".format(number, last_di))
 elif last_di < 6 and last_di != 0:
-	print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_di))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_di))
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 from string import ascii_lowercase
 for i in range(97, 123):
-	print('{}'.format(chr(i)), end="")
+    print('{}'.format(chr(i)), end="")
