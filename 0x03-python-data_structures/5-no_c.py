eb79fc9ab16b8922ba4f5660de0325b7fab6f6c2
Carmelleonie <sagboleonie7@gmail.com>
2023-12-03 00:18:33 +0100

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-	char_cC = "cC"
-	my_ne = ""
-	for char in my_string:
-		if char not in char_cC:
-			my_ne = my_ne + char
-	return my_ne
+    char_cC = "cC"
+    my_ne = ""
+    for char in my_string:
+        if char not in char_cC:
+            my_ne = my_ne + char
+    return my_ne
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-	for idx in range(len(my_list) - 1):
-		if not my_list:
-			return None
-		else:
-			my_list.sort()
-			return my_list[-1]
+    for idx in range(len(my_list) - 1):
+        if not my_list:
+            return None
+        else:
+            my_list.sort()
+            return my_list[-1]
