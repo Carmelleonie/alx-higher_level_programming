596c7ed549a3924c4a16f963d472614ac57a9bc6
Carmelleonie <sagboleonie7@gmail.com>
2023-12-06 19:40:05 +0100

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    newlist = []
-    for i in set_1:
-        for j in set_2:
-            if i is not j:
-                newlist.append(i)
-    return newlist
+    new_t = [y for x in (set_1, set_2) for y in x]
+    new_list = list(filter(lambda i: new_t.count(i) == 1, new_t))
+    return new_list
