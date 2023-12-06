1f595250553b90bd2cdd13081b41076f722735bd
Carmelleonie <sagboleonie7@gmail.com>
2023-12-06 18:25:16 +0100

    1-main.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if i is j:
-                return [i]
+    element = [x for x in set_1 for y in set_2 if x == y]
+    return element
