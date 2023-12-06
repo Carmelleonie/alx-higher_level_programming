8f23766bd9aa0f651dc0150551272b8a21177784
Carmelleonie <sagboleonie7@gmail.com>
2023-12-06 17:22:35 +0100

    0x04-python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-	for i in range(len(my_list) -1):
-		if my_list[i] == my_list[i + 1]:
-			
+    result = 0
+    for i in (set(my_list)):
+        result += i
+    return result
