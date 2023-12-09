8d49f715c1894a3b2dbb5f18559d62620ce46165
Carmelleonie <sagboleonie7@gmail.com>
2023-12-09 16:24:26 +0100

    0x04

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,9 @@
-#!/usr/bin/pyhton3
-from functools import reduce
+#!/usr/bin/python3
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    #tuple_2 = [tuple_t[1] for tuple_t in my_list]
-    div = reduce(lambda a, b: a + b, list(map(lambda tuple_t: tuple_t[1], my_list)))
-    result = reduce(lambda x, y: x + y, (list(map(lambda tuple_t: tuple_t[0] * tuple_t[1], my_list)))) / div
-    return result
+    mark = 0; coef = 0
+    for tuple_t in my_list:
+        mark += (tuple_t[0] * tuple_t[1])
+        coef += tuple_t[1]
+    return mark / coef
