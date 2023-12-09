b97db3d4bd5cf6dafb9e723868f58f5c67b1d409
Carmelleonie <sagboleonie7@gmail.com>
2023-12-09 08:13:50 +0100

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    mark = 0
+    coef = 0
+    for i in my_list:
+        mark += (i[0] * i[1])
+        coef += i[1]
+        
+    return mark / coef
