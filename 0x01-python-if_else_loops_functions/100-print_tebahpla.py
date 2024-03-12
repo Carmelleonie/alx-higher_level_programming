4559c9f70ed572ee5b4060c72e9a99c80b18a7bc
Carmelleonie <sagboleonie7@gmail.com>
2024-03-12 22:43:28 +0100

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -2):
-	print(chr(i), end="")
+j = 0
+for i in range(122, 96, -1):
+    print("{}".format(chr(i - j)), end="")
+    j = 32 if j == 0 else 0
