41ef8df516e667cb19f8dba53b0e18475026debc
Carmelleonie <sagboleonie7@gmail.com>
2024-03-19 22:03:41 +0100

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,40 +7,77 @@
 *Return: The lenght of the list
 */
 
-int list_len(listint_t **head)
+void reverse(listint_t **head)
 {
-	int len = 0;
+	listint_t *prev = NULL, *curr, *next = NULL;
 
-	while (head != NULL && (*head)->next != NULL)
+	curr = *head;
+
+	while (curr != NULL)
 	{
-		len++;
-		(*head) = (*head)->next;
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
 	}
-	return (len);
+	(*head) = prev;
 }
+
+
+int compare_element(listint_t **head1, listint_t **head2)
+{
+	listint_t *tmp1 = *head1, *tmp2 = *head2;
+
+	while (tmp1 && tmp2)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			return (1);
+		}
+		return (0);
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next;
+	}
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+
 
 int is_palindrome(listint_t **head)
 {
-	int len = list_len(head);
-	int i = 0, j = 0;
-	listint_t *first, *last;
+	listint_t *fast_ptr = *head, *slow_ptr = *head, *prev_to_slow = NULL,
+	*second_half, *mid_node;
 
-	first = last = *head;
-	while (i != len / 2)
+	while (fast_ptr && fast_ptr->next)
 	{
-		for (j = 0; j < i; j++)
-		{
-			first = first->next;
-		}
-		for (j = 0; j < (len - (i + 1)); j++)
-		{
-			last = last->next;
-		}
-		if (first->n != last->n)
-		{
-			return (0);
-		}
-		i++;
+		fast_ptr = fast_ptr->next->next;
+		prev_to_slow = slow_ptr;
+		slow_ptr = slow_ptr->next;
 	}
-	return (1);
+	if (fast_ptr)
+	{
+		mid_node = slow_ptr;
+		slow_ptr = slow_ptr->next;
+	}
+	second_half = slow_ptr;
+	prev_to_slow = NULL;
+
+	reverse(&second_half);
+
+	int comp = compare_element(head, &second_half);
+
+	if (mid_node != NULL) 
+	{ 
+		prev_to_slow->next = mid_node; 
+		mid_node->next = second_half; 
+	} 
+	else
+	{
+		prev_to_slow->next = second_half; 
+	}
+	return (comp);
 }
